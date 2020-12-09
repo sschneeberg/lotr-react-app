dbcb949ac4912559b45b44d3cffc63244197e8c2
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-09 09:42:31 -0500

    Change to spread operator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from "./lotr.png";
-import Movie from "./Movie";
-import "./App.css";
+import logo from './lotr.png';
+import Movie from './Movie';
+import './App.css';
 
 function App() {
-  const movies = [
-    { title: "The Fellowship of the Ring", hours: "2", minutes: "58" },
-    { title: "The Two Towers", hours: "2", minutes: "59" },
-    { title: "The Return of the King", hours: "3", minutes: "21" },
-  ];
+    const movies = [
+        { title: 'The Fellowship of the Ring', hours: '2', minutes: '58' },
+        { title: 'The Two Towers', hours: '2', minutes: '59' },
+        { title: 'The Return of the King', hours: '3', minutes: '21' }
+    ];
 
-  const displayFilms = movies.map((movie, index) => {
-    return <Movie movie={movie} key={index} />;
-  });
+    const displayFilms = movies.map((movie, index) => {
+        return <Movie {...movie} key={index} />;
+    });
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="App">{displayFilms}</div>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="App">{displayFilms}</div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
